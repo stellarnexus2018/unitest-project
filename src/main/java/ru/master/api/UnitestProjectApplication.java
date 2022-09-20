4d9208946d3878067edfbb746c1b96a85b20e75d
igoryasha <stellarnexus@gmail.com>
2022-09-20 22:38:39 +0300

    Init commit

---
+++ src/main/java/ru/master/api/UnitestProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.master.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitestProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnitestProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/master/api/UnitestProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.master.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitestProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+    System.out.println("проверка");
+  }
+
+}
